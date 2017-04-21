b488ccee48f46de9ac24b83c0b7fdd5c1dedda65
lby3 <1192235026@qq.com>
2017-04-21 16:10:17 +0800

    123

--- public/javascripts/angular1.js
+++ public/javascripts/angular1.js
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title></title>
-	</head>
-	<body>
-	</body>
-</html>
+//<!DOCTYPE html>
+//<html>
+//	<head>
+//		<meta charset="UTF-8">
+//		<title></title>
+//	</head>
+//	<body>
+//	</body>
+//</html>
