e24bf00abecb36f0b3b7af0036833dca63585b0a
lby3 <1192235026@qq.com>
2017-04-24 18:36:47 +0800

    12

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -56,6 +57,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 app.listen('3333', function() {
 	console.log('server start')
 })
